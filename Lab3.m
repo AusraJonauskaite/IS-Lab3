ae9b8439dec8a4b2446f938bc44259730ce00551
AusraJonauskaite <ausra.jonauskaite@gmail.com>
2021-11-17 12:14:21 +0200

    Padaryta pagrindine dalis

--- Lab3.m
+++ Lab3.m
@@ -3,26 +3,24 @@ clc;
 clear all;
 close all;
 % Pagrindinė užduotis
-%1.Duomenu paruosimas
+%Duomenu paruosimas
 %įėjime paduodamas vektorius x
-x =[0.1:1/22:1];
+x =[0:1/22:1];
 %vienas išėjimas 
 d = ((1 + 0.6*sin(2*pi*x/0.7))+(0.3*sin(2*pi*x)))/2;
 figure(1)
 plot(x,d,'k')
 grid on;
-
 %centru c ir spinduliu r reiksmes
 %c pasirenkame ekstremumus is grafiko
-c1 = 0.2;
-c2 = 0.9;
+c1 = 0.20;
+c2 = 0.91;
 r1 = 0.15;
 r2 = 0.15;
-
 %isejimo sluoksnio rysio svoriu parinkimas
-w1 = rand(1);
-w2 = rand(1);
-b = rand(1);
+w1 = rand(1)*0.1;
+w2 = rand(1)*0.1;
+b = rand(1)*0.1;
 n = 0.1;
 %Tinklo apmokymas
 for i = 1:1000
@@ -37,3 +35,14 @@ for i = 1:1000
         b = b + n*e;
     end
 end
+X = [0:1/22:1];
+Y = zeros(1, length(X));
+for index=1:length(X)
+    F1=exp(-(((X(index)-c1)^2)/(2*r1^2)));
+    F2=exp(-(((X(index)-c2)^2)/(2*r2^2)));
+    y=F1*w1+F2*w2+b;
+    Y(index)=y;
+end
+hold on 
+plot(X,Y,'rx')
+hold off
